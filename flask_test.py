f1ce77ae4f6ad6fb24923d1b2651d3ef79181073
void-echo <beihai142857@outlook.com>
2023-07-13 09:03:09 +0800

    Merge with Yanwen Sun

--- flask_test.py
+++ flask_test.py
@@ -64,7 +64,7 @@ def detect_motion(frameCount):
         # read the next frame from the video stream, resize it,
         # convert the frame to grayscale, and blur it
         frame = vs.read()
-        frame = imutils.resize(frame, width=400)
+        frame = imutils.resize(frame, width=580)
         gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
         gray = cv2.GaussianBlur(gray, (7, 7), 0)
 
